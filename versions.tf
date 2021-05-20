f6306b13e0051d40cfaab589d4481382f756e9c9
Roger Chao <rogman@gmail.com>
2021-05-19 23:08:40 -0700

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    infoblox = {
+      source = "usr/local/bin"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  required_version = ">= 0.13"
+}
