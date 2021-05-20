eee04df0e0a8ba9c3cb6405a021fd4c592da86eb
Roger Chao <rogman@gmail.com>
2021-05-19 18:12:28 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,11 @@
 
 provider "infoblox" {}
 
-variable "super_secret" {}
-
-resource "random_pet" "server" {
-  prefix = var.super_secret
-}
-
 resource "null_resource" "delay" {
   provisioner "local-exec" {
     command = "sleep 360"
   }
 }
 
-resource "null_resource" "env" {
-  provisioner "local-exec" {
-    command = "env"
-  }
-}
 
-output "super_secret" {
-  value = var.super_secret
-  sensitive   = true
-}
-
-output "super_secret_pet" {
-  value = random_pet.server.id
-}
 
